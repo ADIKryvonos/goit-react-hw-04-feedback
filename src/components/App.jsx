b046b7605ab8e4337532ddabe4a73d161ed10efd
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-03-14 13:09:36 +0200

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Wrap } from './App.styled';
 import { Section } from './FeedbackContainer/FeedbackContainer';
 import { Statistics } from './Statistics/Statistics';
